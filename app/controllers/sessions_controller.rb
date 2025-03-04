b47cbc55f37007a6bd1bc84271586dce2801dd05
Adrian Rodriguez <ar9897.r@gmail.com>
2025-03-03 19:41:09 -0600

    something

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
   def create
-    member = Member.find_by(email: params[:email])
-    if member && member.authenticate(params[:password])
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
       jwt = JWT.encode(
         {
-          member_id: member.id, # the data to encode
+          user_id: user.id, # the data to encode
           exp: 24.hours.from_now.to_i # the expiration time
         },
         Rails.application.credentials.fetch(:secret_key_base), # the secret key
         "HS256" # the encryption algorithm
       )
-      render json: { jwt: jwt, email: member.email, member_id: member.id }, status: :created
+      render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
     else
       render json: {}, status: :unauthorized
     end
