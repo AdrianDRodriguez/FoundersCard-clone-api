3149064fde633124adea054c2d1e304d52f134a4
Adrian Rodriguez <ar9897.r@gmail.com>
2025-03-25 21:19:00 -0500

    fixing cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-  origins "localhost:5173", "localhost:3000"
+  origins "localhost:5173", "localhost:3000", "founderscard-clone-frontend.onrender.com"
     resource "*", headers: :any, credentials: true, methods: [ :get, :post, :patch, :put, :delete ]
   end
 end
